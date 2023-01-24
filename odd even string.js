2905b3632589746e93d34511dc6444e37488ce3c
Kira Beloludinova <102582189+KiraBelo@users.noreply.github.com>
2023-01-24 22:26:13 +0300

    Create odd even string.js

---
+++ odd even string.js
@@ -0,0 +1,13 @@
+/*Given a string s. You have to return another string such that even-indexed and odd-indexed characters of s are grouped and groups are space-separated (see sample below)
+
+Note: 
+0 is considered to be an even index. 
+All input strings are valid with no spaces
+input: 'CodeWars'
+output 'CdWr oeas'*/
+
+const sortMyString = s => {
+    let even = s.split('').filter((v, i) => i % 2 === 0).join('')
+    let odd = s.split('').filter((v, i) => i % 2 !== 0).join('')
+    return even + ' ' + odd
+}
